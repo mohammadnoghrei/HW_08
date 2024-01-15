96c8da4e65062da1f61c682569b1faca430e93c2
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-15 22:38:23 +0330

    create user class fields and methods

---
+++ src/main/java/model/User.java
@@ -0,0 +1,78 @@
+package model;
+
+public class User {
+    private int user_id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int user_id, String name, String username, String email, String password) {
+        this.user_id = user_id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
