495172e7c456321e77f0f6edd39c88c6b5ad246d
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-15 22:28:31 +0330

    add query of create tables in tables.sql

---
+++ src/main/java/sql/tables.sql
@@ -0,0 +1,36 @@
+create table if not exists shareholders(
+                                           shareholderId serial primary key ,
+                                           shareholderNname varchar(255),
+    phoneNumber int,
+    nationalCode int
+    );
+create table if not exists brand(
+                                    brandId serial primary key ,
+                                    brandName varchar(255),
+    website varchar(255),
+    description varchar(255)
+    );
+create  table if not exists shareholder_brand(
+    shareholderId int references shareholders(shareholderId),
+    brandId int references brand(brandId)
+    );
+create table if not exists category(
+                                       categoryId serial primary key ,
+                                       categoryName varchar(255),
+    description varchar(255)
+    );
+create table if not exists product(
+                                      productId serial primary key ,
+                                      productName varchar(255),
+    createDate DATE,
+    categoryId int references category(categoryId),
+    brandId int references brand(brandId)
+    );
+
+
+create table if not exists users(
+                                    userID serial primary key ,
+                                    name varchar(255),
+    email varchar(255),
+    password varchar(255)
+    );
