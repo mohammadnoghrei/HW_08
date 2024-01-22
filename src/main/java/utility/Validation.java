8a0db47704ab56afecc6fba3e84e856aefe29b0c
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-23 00:36:51 +0330

    crete two methods validatiln

---
+++ src/main/java/utility/Validation.java
@@ -0,0 +1,67 @@
+package utility;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static boolean emailvalidation(String email) {
+        String reg="^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$";
+        Pattern p = Pattern.compile(reg);
+        Matcher m = p.matcher(email);
+
+        return m.matches();
+    }
+public static boolean phonenumbervalidation(String phonenumber){
+
+    String reg="^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}";
+    Pattern p = Pattern.compile(reg);
+    Matcher m = p.matcher(phonenumber);
+
+    return m.matches();
+}
+    public static boolean validateMelliCode(String melliCode) {
+
+        String[] identicalDigits = {"0000000000", "1111111111", "2222222222", "3333333333", "4444444444", "5555555555", "6666666666", "7777777777", "8888888888", "9999999999"};
+
+        if (melliCode.trim().isEmpty()) {
+
+            System.out.println("National Code is empty");
+            return false; // National Code is empty
+
+        } else if (melliCode.length() != 10) {
+
+            System.out.println("National Code must be exactly 10 digits");
+            return false; // National Code is less or more than 10 digits
+
+        } else if (Arrays.asList(identicalDigits).contains(melliCode)) {
+
+            System.out.println("MelliCode is not valid (Fake MelliCode)");
+            return false; // Fake National Code
+
+        } else {
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++) {
+                sum += Character.getNumericValue(melliCode.charAt(i)) * (10 - i);
+            }
+
+            int lastDigit;
+            int divideRemaining = sum % 11;
+
+            if (divideRemaining < 2) {
+                lastDigit = divideRemaining;
+            } else {
+                lastDigit = 11 - (divideRemaining);
+            }
+
+            if (Character.getNumericValue(melliCode.charAt(9)) == lastDigit) {
+                System.out.println("MelliCode is valid");
+                return true;
+            } else {
+                System.out.println("MelliCode is not valid");
+                return false; // Invalid MelliCode
+            }
+        }
+    }
+}
