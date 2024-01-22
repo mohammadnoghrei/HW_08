87d456b8fd2deb5650c9606d434bb5163180468c
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-23 00:40:31 +0330

    add Brandservice and brandrepository toApplication context

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,7 +1,12 @@
 package utility;
 
 import connection.Jdbcconnection;
+import repository.Brandrepository;
+import repository.ShareholderRepository;
 import repository.UserReposority;
+import service.Brandservice;
+import service.ShareholderService;
+import service.Userservice;
 
 import java.sql.Connection;
 
@@ -9,10 +14,24 @@ public class ApplicationContext {
 
     private static final Connection CONNECTION ;
     private static final UserReposority USER_REPOSORITY;
-
+    private static final Userservice USERSERVICE;
+    private static final ShareholderRepository SHAREHOLDER_REPOSITORY;
+    private static final ShareholderService SHAREHOLDER_SERVICE;
+    private static final Brandrepository BRANDREPOSITORY;
+    private static final Brandservice BRANDSERVICE;
     static {
         CONNECTION = Jdbcconnection.getConnection();
         USER_REPOSORITY = new UserReposority(CONNECTION);
-
+        USERSERVICE = new Userservice(USER_REPOSORITY);
+        SHAREHOLDER_REPOSITORY = new ShareholderRepository(CONNECTION);
+        SHAREHOLDER_SERVICE =new ShareholderService(SHAREHOLDER_REPOSITORY);
+        BRANDREPOSITORY =new Brandrepository(CONNECTION);
+        BRANDSERVICE=new Brandservice(BRANDREPOSITORY);
     }
+    public static Userservice getUserService(){
+        return USERSERVICE;
+    }
+public static ShareholderService getShareholderService(){return SHAREHOLDER_SERVICE;}
+    public static Brandservice getBrandservice(){return BRANDSERVICE;}
+
 }
