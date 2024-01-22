157442630c631afcaa07bedd23517307b95a015d
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-23 00:41:00 +0330

    create brand service class

---
+++ src/main/java/service/Brandservice.java
@@ -0,0 +1,11 @@
+package service;
+
+import repository.Brandrepository;
+
+public class Brandservice {
+    Brandrepository brandrepository;
+
+    public Brandservice(Brandrepository brandrepository) {
+        this.brandrepository = brandrepository;
+    }
+}
